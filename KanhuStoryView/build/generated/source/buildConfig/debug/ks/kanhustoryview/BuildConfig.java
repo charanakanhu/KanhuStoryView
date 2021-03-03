70edcd07cc1defc14250e04ae793e12644ac3926
Kanhu Charana Rath <35791879+charanakanhu@users.noreply.github.com>
2021-03-04 00:07:14 +0530

    Add files via upload

---
+++ KanhuStoryView/build/generated/source/buildConfig/debug/ks/kanhustoryview/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ks.kanhustoryview;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "ks.kanhustoryview";
+  public static final String BUILD_TYPE = "debug";
+}
